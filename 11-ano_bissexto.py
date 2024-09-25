5e22d5b2c1218e551e0798663fc573d88be5c859
eduhernandes2c <eduhernandes2c@firemail.com.br>
2024-09-25 11:01:47 -0300

    Create 11-ano_bissexto.py

---
+++ 11-ano_bissexto.py
@@ -0,0 +1,7 @@
+# Algoritmo do ano bissexto
+# O usuário digitará o ano e o programa informará se o ano
+# é ou não é bissexto.
+# Regras do ano bissexto:
+# - O ano deve ser múltiplo de 4
+# - O ano não deve ser múltiplo de 100
+# - Exceto anos múltiplos de 400
