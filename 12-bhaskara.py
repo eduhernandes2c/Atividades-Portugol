3f738ec9450827545ea22307448b230b42e19c33
eduhernandes2c <eduhernandes2c@firemail.com.br>
2024-09-25 11:02:19 -0300

    Create 12-bhaskara.py

---
+++ 12-bhaskara.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que calcula as raízes de uma
+# equação quadrática utilizando a fórmula de Bhaskara.
+# 1- Solicite ao usuário para inserir os valores dos
+# coeficientes a, b e c.
+# 2- Calcule o discriminante.
+# 3- Verifique o valor do discriminante:
+# Se delta for negativo, imprima "A equação não possui raízes reais."
+# Se delta for igual a zero, calcule e imprima a única raiz real.
+# Se delta for positivo, calcule e imprima as duas raízes reais.
